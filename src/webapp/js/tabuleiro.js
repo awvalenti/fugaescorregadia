ee48b67818b58e24f49a855c282c0a1ed2a5ad8d
Fi <awvalenti@gmail.com>
2011-08-06 21:18:35 -0300

    Aumento de desempenho na criação do tabuleiro

--- src/webapp/js/tabuleiro.js
+++ src/webapp/js/tabuleiro.js
@@ -3,26 +3,16 @@ function Tabuleiro(nLinhas, nColunas, fase, personagem) {
 	this.nLinhas = nLinhas;
 	this.nColunas = nColunas;
 
-	function criarElemento(elemento) {
-		return $('<div>')
-			.data('elemento', Elemento[elemento.nome])
-			.addClass('celula ' + elemento.nome);
-	}
-
 	var divPrincipal = $('div#principal').hide().empty();
 
-	con.debug('Criando elementos...');
-	for (var linha = 0; linha < this.nLinhas; ++linha) {
-		this.matriz[linha] = [];
-		for (var coluna = 0; coluna < this.nColunas; ++coluna) {
-			var divElemento = criarElemento(Elemento.NADA).addClass('linha' + linha + ' coluna' + coluna);
-			divPrincipal.append(divElemento);
-			this.matriz[linha][coluna] = divElemento;
-		}
+	function acrescentarElemento(el) {
+		var divElemento =
+			$('<div>')
+				.addClass('celula')
+				.addClass(el.nome)
+				.data('elemento', el);
+		return divElemento.appendTo(divPrincipal);
 	}
-
-	divPrincipal.append(criarElemento(Elemento.PERSONAGEM));
-	con.debug('Criou.');
 
 	var stringFase = $('#meta .fase').eq(fase).text().replace(/\s+/g, '');
 	var tamanhoStringFase = stringFase.length;
@@ -31,9 +21,11 @@ function Tabuleiro(nLinhas, nColunas, fase, personagem) {
 		throw 'Tamanho da matriz inesperado: ' + tamanhoStringFase + '. Esperado: ' + tamanhoEsperado;
 	}
 
+	con.debug('Criando elementos...');
 	var mapaSohUmPorFase = {};
 	var indice = 0;
 	for (var linha = 0; linha < this.nLinhas; ++linha) {
+		this.matriz[linha] = [];
 		for (var coluna = 0; coluna < this.nColunas; ++coluna, ++indice) {
 			var caractereElemento = stringFase.charAt(indice);
 			var elemento = MAPA_CONSTRUCAO_FASE[caractereElemento];
@@ -56,22 +48,27 @@ function Tabuleiro(nLinhas, nColunas, fase, personagem) {
 			switch (elemento) {
 			case Elemento.PERSONAGEM:
 				personagem.setPosicaoInicial(linha, coluna);
-				this.setAux(linha, coluna, Elemento.NADA);
+				this.matriz[linha][coluna] = acrescentarElemento(Elemento.NADA);
 				break;
 			default:
-				this.setAux(linha, coluna, elemento);
+				this.matriz[linha][coluna] = acrescentarElemento(elemento);
 			}
 		}
 	}
-	con.debug('ok Tabuleiro');
+
+	acrescentarElemento(Elemento.PERSONAGEM);
+
+	con.debug('Criou.');
 }
 
-Tabuleiro.prototype.setAux = function(linha, coluna, elemento) {
+Tabuleiro.prototype.setAux = function(linha, coluna, novoElemento) {
 	var divElemento = this.matriz[linha][coluna];
+	var elementoAntigo = divElemento.data('elemento');
+
 	divElemento
-		.removeClass(divElemento.data('elemento'))
-		.data('elemento', Elemento[elemento.nome])
-		.addClass(elemento.nome);
+		.removeClass(elementoAntigo.nome)
+		.addClass(novoElemento.nome)
+		.data('elemento', novoElemento);
 };
 
 Tabuleiro.prototype.set = function(posicao, objeto) {
