b1a49b4ece5b833a3a621f93ac8a165314038c88
André Willik Valenti <awvalenti@gmail.com>
2013-11-13 10:07:40 -0200

    Primeiro commit da nova fase do projeto. Jasmine Spec Runner em ordem.

---
+++ src/js/spec-runner.js
@@ -0,0 +1,24 @@
+'use strict';
+
+requirejs(['lib/non-amd/jasmine-1.3.1/jasmine'], function() {
+requirejs(['lib/non-amd/jasmine-1.3.1/jasmine-html'], function() {
+
+requirejs([
+],
+function() {
+  var jasmineEnv = jasmine.getEnv();
+  jasmineEnv.updateInterval = 1000;
+
+  var htmlReporter = new jasmine.HtmlReporter();
+
+  jasmineEnv.addReporter(htmlReporter);
+
+  jasmineEnv.specFilter = function(spec) {
+    return htmlReporter.specFilter(spec);
+  };
+
+  jasmineEnv.execute();
+});
+
+});
+});
