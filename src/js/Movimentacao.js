cc40b6545d452dde2289838fb93018d144d1f4e7
André Willik Valenti <awvalenti@gmail.com>
2013-11-17 19:42:15 -0200

    Movimentacao agora está sem estado

--- src/js/Movimentacao.js
+++ src/js/Movimentacao.js
@@ -7,43 +7,22 @@ function(
   'use strict';
 
   function Movimentacao(params) {
-    assert.args(params.posicao, 'function', params.posicaoPersonagem, 'object', params.largura, 'number', params.altura, 'number');
+    assert.args(params.posicao, 'function', params.largura, 'number', params.altura, 'number');
 
     this._posicao = params.posicao;
     this._largura = params.largura;
     this._altura = params.altura;
-    this._posicaoPersonagem = params.posicaoPersonagem;
   }
 
-  Movimentacao.prototype.posicaoPersonagem = function() {
-    return this._posicaoPersonagem;
-  };
-
-  Movimentacao.prototype.andarPara = function(direcao) {
-    while (this._posicaoPersonagem.somar(direcao).estaContidaEm(this._altura, this._largura)) {
-      this._posicaoPersonagem = this._posicaoPersonagem.somar(direcao);
+  Movimentacao.prototype.movimentarPersonagem = function(posicaoPersonagem, direcao) {
+    var novaPosicao = posicaoPersonagem;
+    while (novaPosicao.somar(direcao).estaContidaEm(this._altura, this._largura)) {
+      novaPosicao = novaPosicao.somar(direcao);
     }
+
+    return novaPosicao;
   };
 
-  function MovimentacaoFactory() {}
-
-  MovimentacaoFactory.paraTabuleiro = function(largura, altura) {
-    return {
-      comPersonagemEm: function(posicaoPersonagem) {
-        return {
-          comDependencias: function(dependencias) {
-            return new Movimentacao({
-              posicao: dependencias.posicao,
-              posicaoPersonagem: posicaoPersonagem,
-              largura: largura,
-              altura: altura
-            });
-          }
-        }
-      }
-    };
-  };
-
-  return MovimentacaoFactory;
+  return Movimentacao;
 
 });
--- src/js/spec/MovimentacaoSpec.js
+++ src/js/spec/MovimentacaoSpec.js
@@ -17,36 +17,28 @@ function(
   'use strict';
 
   describe('Movimentacao', function() {
-    var mov = null, posicao = null;
+    var mov = null, posicao = null, doisDois = null;
 
     beforeEach(function() {
       posicao = Posicao.criarFabrica();
-      mov = Movimentacao.paraTabuleiro(5, 5).comPersonagemEm(posicao(2, 2)).comDependencias({ posicao: posicao });
-    });
-
-    it('deve construir Movimentacao', function() {
-      expect(mov).toBeTruthy();
-      expect(mov.posicaoPersonagem()).toBe(posicao(2, 2));
+      doisDois = posicao(2, 2);
+      mov = new Movimentacao({ largura: 5, altura: 5, posicao: posicao });
     });
 
     it('deve permitir andar para BAIXO', function() {
-      mov.andarPara(BAIXO);
-      expect(mov.posicaoPersonagem()).toBe(posicao(4, 2));
+      expect(mov.movimentarPersonagem(doisDois, BAIXO)).toBe(posicao(4, 2));
     });
 
     it('deve permitir andar para CIMA', function() {
-      mov.andarPara(CIMA);
-      expect(mov.posicaoPersonagem()).toBe(posicao(0, 2));
+      expect(mov.movimentarPersonagem(doisDois, CIMA)).toBe(posicao(0, 2));
     });
 
     it('deve permitir andar para DIREITA', function() {
-      mov.andarPara(DIREITA);
-      expect(mov.posicaoPersonagem()).toBe(posicao(2, 4));
+      expect(mov.movimentarPersonagem(doisDois, DIREITA)).toBe(posicao(2, 4));
     });
 
     it('deve permitir andar para ESQUERDA', function() {
-      mov.andarPara(ESQUERDA);
-      expect(mov.posicaoPersonagem()).toBe(posicao(2, 0));
+      expect(mov.movimentarPersonagem(doisDois, ESQUERDA)).toBe(posicao(2, 0));
     });
 
   });
