a0b24b44ee7e768cbb8c97b2caa678c8ed096c4b
André Willik Valenti <awvalenti@gmail.com>
2013-11-17 20:15:18 -0200

    Contraindo specs de direções em uma só

--- src/js/spec/MovimentacaoSpec.js
+++ src/js/spec/MovimentacaoSpec.js
@@ -25,22 +25,14 @@ function(
       mov = new Movimentacao({ largura: 5, altura: 5, posicao: posicao });
     });
 
-    it('deve permitir andar para BAIXO', function() {
-      expect(mov.movimentarPersonagem(doisDois, BAIXO)).toBe(posicao(4, 2));
+    describe('livre', function() {
+      it('deve permitir andar para as quatro direcoes', function() {
+        expect(mov.movimentarPersonagem(doisDois, BAIXO)).toBe(posicao(4, 2));
+        expect(mov.movimentarPersonagem(doisDois, CIMA)).toBe(posicao(0, 2));
+        expect(mov.movimentarPersonagem(doisDois, DIREITA)).toBe(posicao(2, 4));
+        expect(mov.movimentarPersonagem(doisDois, ESQUERDA)).toBe(posicao(2, 0));
+      });
     });
-
-    it('deve permitir andar para CIMA', function() {
-      expect(mov.movimentarPersonagem(doisDois, CIMA)).toBe(posicao(0, 2));
-    });
-
-    it('deve permitir andar para DIREITA', function() {
-      expect(mov.movimentarPersonagem(doisDois, DIREITA)).toBe(posicao(2, 4));
-    });
-
-    it('deve permitir andar para ESQUERDA', function() {
-      expect(mov.movimentarPersonagem(doisDois, ESQUERDA)).toBe(posicao(2, 0));
-    });
-
   });
 
 });
