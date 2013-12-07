df6f5a2ad9a45857207d4fed3de1c6fcb787039e
André Willik Valenti <awvalenti@gmail.com>
2013-12-07 17:12:07 -0200

    Detalhes

--- src/js/Direcao.js
+++ src/js/Direcao.js
@@ -1,14 +1,16 @@
 define([
+  'enumerate'
 ],
 function(
+  enumerate
 ) {
   'use strict';
 
-  return {
-    ESQUERDA: { linha: 0, coluna: -1 },
-    CIMA:     { linha: -1, coluna: 0 },
-    DIREITA:  { linha: 0, coluna: +1 },
-    BAIXO:    { linha: +1, coluna: 0 },
-  };
+  return enumerate(
+    'ESQUERDA', { linha: 0, coluna: -1 },
+    'CIMA',     { linha: -1, coluna: 0 },
+    'DIREITA',  { linha: 0, coluna: +1 },
+    'BAIXO',    { linha: +1, coluna: 0 }
+  );
 
 });
--- src/js/Elemento.js
+++ src/js/Elemento.js
@@ -19,12 +19,12 @@ function(
       permiteSair: function() { return true; },
     },
     'VAZIO',
-    'OBSTACULO',  { permiteEntrarVindoDe: function() { return false; } },
-    'SETA_CIMA',  { permiteEntrarVindoDe: function(direcao) { return direcao === CIMA; } },
-    'SETA_BAIXO', { permiteEntrarVindoDe: function(direcao) { return direcao === BAIXO; } },
+    'OBSTACULO',     { permiteEntrarVindoDe: function() { return false; } },
+    'SETA_CIMA',     { permiteEntrarVindoDe: function(direcao) { return direcao === CIMA; } },
+    'SETA_BAIXO',    { permiteEntrarVindoDe: function(direcao) { return direcao === BAIXO; } },
     'SETA_ESQUERDA', { permiteEntrarVindoDe: function(direcao) { return direcao === ESQUERDA; } },
-    'SETA_DIREITA', { permiteEntrarVindoDe: function(direcao) { return direcao === DIREITA; } },
-    'COLA', { permiteSair: function() { return false; } }
+    'SETA_DIREITA',  { permiteEntrarVindoDe: function(direcao) { return direcao === DIREITA; } },
+    'COLA',          { permiteSair: function() { return false; } }
   );
 
 });
