d670dddb5db7d2713e9607cb27ac77aee3085b83
André Willik Valenti <awvalenti@gmail.com>
2013-12-08 18:12:57 -0200

    MovimentacaoSpec melhorada

--- src/js/spec/MovimentacaoSpec.js
+++ src/js/spec/MovimentacaoSpec.js
@@ -26,9 +26,11 @@ function(
 ) {
   'use strict';
 
-  describe('Movimentacao', function() {
-    function tabuleiroVazio() { return VAZIO; }
+  function tabuleiroVazio()         { return VAZIO; }
+  function bloqueioEm10_15(posicao) { return posicao.eh(10, 15) ? OBSTACULO : VAZIO; }
+  function colaEm10_15(posicao)     { return posicao.eh(10, 15) ? COLA : VAZIO; }
 
+  describe('Movimentacao', function() {
     var repoPosicoes = null, doisDois = null, mov = null;
 
     beforeEach(function() {
@@ -39,30 +41,26 @@ function(
 
     describe('livre', function() {
       it('deve encerrar movimento somente nas extremidades do tabuleiro', function() {
-        expect(mov.movimentarPersonagem(doisDois, BAIXO, tabuleiroVazio)).toBe(repoPosicoes.obter(20, 10));
-        expect(mov.movimentarPersonagem(doisDois, CIMA, tabuleiroVazio)).toBe(repoPosicoes.obter(0, 10));
-        expect(mov.movimentarPersonagem(doisDois, DIREITA, tabuleiroVazio)).toBe(repoPosicoes.obter(10, 20));
+        expect(mov.movimentarPersonagem(doisDois, BAIXO,    tabuleiroVazio)).toBe(repoPosicoes.obter(20, 10));
+        expect(mov.movimentarPersonagem(doisDois, CIMA,     tabuleiroVazio)).toBe(repoPosicoes.obter(0, 10));
+        expect(mov.movimentarPersonagem(doisDois, DIREITA,  tabuleiroVazio)).toBe(repoPosicoes.obter(10, 20));
         expect(mov.movimentarPersonagem(doisDois, ESQUERDA, tabuleiroVazio)).toBe(repoPosicoes.obter(10, 0));
       });
     });
 
     describe('com bloqueio', function() {
-      function bloqueioEm10_15(posicao) { return posicao.eh(10, 15) ? OBSTACULO : VAZIO; }
-
-      it('deve encerrar movimento uma posicao antes desse elemento', function() {
+      it('deve encerrar movimento uma posicao antes', function() {
         expect(mov.movimentarPersonagem(doisDois, DIREITA, bloqueioEm10_15)).toBe(repoPosicoes.obter(10, 14));
       });
     });
 
     describe('com COLA', function() {
-      function colaEm10_15(posicao) { return posicao.eh(10, 15) ? COLA : VAZIO; }
-
       it('deve encerrar movimento na posicao da COLA', function() {
         expect(mov.movimentarPersonagem(doisDois, DIREITA, colaEm10_15)).toBe(repoPosicoes.obter(10, 15));
       });
 
       it('deve permitir fazer outro movimento a partir da COLA', function() {
-        expect(mov.movimentarPersonagem(repoPosicoes.obter(10, 15), DIREITA, colaEm10_15)).toBe(repoPosicoes.obter(10, 20));
+        expect(mov.movimentarPersonagem(repoPosicoes.obter(10, 15), BAIXO, colaEm10_15)).toBe(repoPosicoes.obter(20, 15));
       });
     });
 
