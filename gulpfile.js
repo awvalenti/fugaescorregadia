be793c3a79d483647c983cff7410eb214c0db650
André Willik Valenti <awvalenti@gmail.com>
2014-06-12 13:28:41 -0300

    Usando throttle do underscore para evitar execução repetida dos testes quando fizer uma alteração em lote em diversos arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@
 
   var gulp = require('gulp');
   var jasmine = require('gulp-jasmine');
+  var _ = require('underscore')._;
 
-  gulp.task('test', function() {
+  gulp.task('test', _(function() {
     gulp.src('src/js/spec/spec-runner-gulp-jasmine.js')
         .pipe(jasmine());
-  });
+  }).throttle(10, { leading: false }));
 
   gulp.task('autotest', ['test'], function() {
     gulp.watch('src/js/**/*.js', ['test']);
