f8fc32e7a228043f409f2dfb85e16d60224cc3f5
André Willik Valenti <awvalenti@gmail.com>
2014-07-06 14:54:48 -0300

    Recuperadas do antigo CompiladorMapa as specs negativas de Mapa

--- src/js/spec/unidade/MapaSpec.js
+++ src/js/spec/unidade/MapaSpec.js
@@ -19,34 +19,49 @@ function(
   describe('Mapa', function() {
     var mapa = null;
 
-    beforeEach(function() {
-      mapa = new Mapa(2, 3,
-        '_ p _' +
-        '_ _ i' +
-        ''
-      );
-    });
-
-    it('deve ser imutavel, permitindo somente copiar a matriz', function() {
-      expect(mapa.copiarMatriz()).toEqual([[VAZIO, PERSONAGEM, VAZIO], [VAZIO, VAZIO, ITEM]]);
-    });
-
-    describe('copias diferentes', function() {
-      var umaCopia = null, outraCopia = null;
-
+    describe('criado corretamente', function() {
       beforeEach(function() {
-        umaCopia = mapa.copiarMatriz();
-        outraCopia = mapa.copiarMatriz();
+        mapa = new Mapa(2, 3,
+          '_ p _' +
+          '_ _ i' +
+          ''
+        );
       });
 
-      it('devem ter mesmo conteudo', function() {
-        expect(umaCopia).toEqual(outraCopia);
+      it('deve ser imutavel, permitindo somente copiar a matriz', function() {
+        expect(mapa.copiarMatriz()).toEqual([[VAZIO, PERSONAGEM, VAZIO], [VAZIO, VAZIO, ITEM]]);
       });
 
-      it('devem ser objetos independentes', function() {
-        expect(umaCopia).not.toBe(outraCopia);
-        expect(umaCopia[0]).not.toBe(outraCopia[0]);
-        expect(umaCopia[1]).not.toBe(outraCopia[1]);
+      describe('copias diferentes', function() {
+        var umaCopia = null, outraCopia = null;
+
+        beforeEach(function() {
+          umaCopia = mapa.copiarMatriz();
+          outraCopia = mapa.copiarMatriz();
+        });
+
+        it('devem ter mesmo conteudo', function() {
+          expect(umaCopia).toEqual(outraCopia);
+        });
+
+        it('devem ser objetos independentes', function() {
+          expect(umaCopia).not.toBe(outraCopia);
+          expect(umaCopia[0]).not.toBe(outraCopia[0]);
+          expect(umaCopia[1]).not.toBe(outraCopia[1]);
+        });
+
+      });
+
+    });
+
+    describe('criado incorretamente', function() {
+      it('deve rejeitar stringMapa com caracteres nao reconhecidos', function() {
+        expect(function() { new Mapa(1, 3, '_ _ k'); }).toThrow('Caractere nao reconhecido: k');
+      });
+
+      it('deve rejeitar stringMapa com tamanho diferente do esperado', function() {
+        expect(function() { new Mapa(1, 2, '_ _ _'); }).toThrow('Esperado: 2 elemento(s). Encontrado: 3 elemento(s).');
+        expect(function() { new Mapa(4, 2, '_ _ _'); }).toThrow('Esperado: 8 elemento(s). Encontrado: 3 elemento(s).');
       });
 
     });
