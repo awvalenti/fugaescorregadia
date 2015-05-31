f350344088f39e48a6a1c8321acb86060d452206
André Willik Valenti <awvalenti@gmail.com>
2015-05-31 14:11:44 -0300

    Main usando leitor de mapas do classpath

--- src/main/java/com/github/awvalenti/fugaescorregadia/programa/MainFugaEscorregadia.java
+++ src/main/java/com/github/awvalenti/fugaescorregadia/programa/MainFugaEscorregadia.java
@@ -1,21 +1,16 @@
 package com.github.awvalenti.fugaescorregadia.programa;
 
+import com.github.awvalenti.fugaescorregadia.componentes.LeitorDeMapaDoClasspath;
 import com.github.awvalenti.fugaescorregadia.interfacegrafica.TabuleiroPanel;
 import com.github.awvalenti.fugaescorregadia.interfacegrafica.TecladoListener;
 import com.github.awvalenti.fugaescorregadia.interfacegrafica.TelaJogo;
 import com.github.awvalenti.fugaescorregadia.nucleo.Mapa;
-import com.github.awvalenti.fugaescorregadia.nucleo.MapaImutavel;
 import com.github.awvalenti.fugaescorregadia.nucleo.Tentativa;
 
 public class MainFugaEscorregadia {
 
 	public static void main(String[] args) {
-		Mapa mapa = new MapaImutavel(""
-				+ "_ _ p _ _ _\n"
-				+ "o _ _ o _ _\n"
-				+ "o _ _ o _ _\n"
-				+ "o _ _ o _ _\n"
-				+ "");
+		Mapa mapa = new LeitorDeMapaDoClasspath().ler("/mapas/01.mapa");
 
 		TabuleiroPanel tabuleiroPanel = new TabuleiroPanel(50, mapa.getNumeroLinhas(), mapa.getNumeroColunas());
 		Tentativa tentativa = new Tentativa(mapa, tabuleiroPanel);
