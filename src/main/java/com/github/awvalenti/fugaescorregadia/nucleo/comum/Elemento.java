569ff5f3cfd5265ffa5e857a01cd94f3445b6a84
André Willik Valenti <awvalenti@gmail.com>
2015-06-19 15:44:19 -0300

    Elemento PORTAL

--- src/main/java/com/github/awvalenti/fugaescorregadia/nucleo/comum/Elemento.java
+++ src/main/java/com/github/awvalenti/fugaescorregadia/nucleo/comum/Elemento.java
@@ -1,23 +1,26 @@
 package com.github.awvalenti.fugaescorregadia.nucleo.comum;
 
+import static com.github.awvalenti.fugaescorregadia.nucleo.comum.AlgoritmoPassagem.*;
 import static com.github.awvalenti.fugaescorregadia.nucleo.comum.Direcao.*;
 
 import java.util.Arrays;
 
 public enum Elemento {
-	VAZIO('-', false, AlgoritmoPassagem.PERMITE_SEMPRE),
+	VAZIO('-', muitosPorMapa(), PERMITE_PASSAGEM),
 
-	PERSONAGEM('p', true, AlgoritmoPassagem.NAO_DEVE_ACONTECER),
+	PERSONAGEM('p', umPorMapa(), SE_TENTAR_PASSAR_OCORRE_ERRO),
 
-	OBSTACULO('o', false, AlgoritmoPassagem.BLOQUEIA_SEMPRE),
+	PORTAL('*', umPorMapa(), PERMITE_PASSAGEM),
 
-	SETA_CIMA('^', false, paraOndeVai -> paraOndeVai == CIMA),
+	OBSTACULO('o', muitosPorMapa(), BLOQUEIA_PASSAGEM),
 
-	SETA_BAIXO('v', false, paraOndeVai -> paraOndeVai == BAIXO),
+	SETA_CIMA('^', muitosPorMapa(), passagemSomenteIndoPara(CIMA)),
 
-	SETA_ESQUERDA('<', false, paraOndeVai -> paraOndeVai == ESQUERDA),
+	SETA_BAIXO('v', muitosPorMapa(), passagemSomenteIndoPara(BAIXO)),
 
-	SETA_DIREITA('>', false, paraOndeVai -> paraOndeVai == DIREITA),
+	SETA_ESQUERDA('<', muitosPorMapa(), passagemSomenteIndoPara(ESQUERDA)),
+
+	SETA_DIREITA('>', muitosPorMapa(), passagemSomenteIndoPara(DIREITA)),
 
 	;
 
@@ -32,8 +35,7 @@ public enum Elemento {
 				.findFirst()
 				.orElseThrow(
 						() -> new IllegalArgumentException(String.format(
-								"Caractere invalido. Codigo: %d. Valor: %c.",
-								(int) c, c)));
+								"Caractere invalido. Codigo: %d. Valor: %c.", (int) c, c)));
 	}
 
 	private Elemento(char caractere, boolean somenteUmPorMapa, AlgoritmoPassagem algoritmoPassagem) {
@@ -59,7 +61,16 @@ public enum Elemento {
 interface AlgoritmoPassagem {
 	boolean permitePasagem(Direcao paraOndeVai);
 
-	static final AlgoritmoPassagem BLOQUEIA_SEMPRE = paraOndeVai -> false;
-	static final AlgoritmoPassagem PERMITE_SEMPRE = paraOndeVai -> true;
-	static final AlgoritmoPassagem NAO_DEVE_ACONTECER = paraOndeVai -> { throw new IllegalStateException(); };
+	static final AlgoritmoPassagem BLOQUEIA_PASSAGEM = paraOndeVai -> false;
+	static final AlgoritmoPassagem PERMITE_PASSAGEM = paraOndeVai -> true;
+	static final AlgoritmoPassagem SE_TENTAR_PASSAR_OCORRE_ERRO = paraOndeVai -> {
+		throw new IllegalStateException();
+	};
+
+	static AlgoritmoPassagem passagemSomenteIndoPara(Direcao d) {
+		return paraOndeVai -> paraOndeVai.equals(d);
+	}
+
+	static boolean muitosPorMapa() { return false; }
+	static boolean umPorMapa() { return true; }
 }
