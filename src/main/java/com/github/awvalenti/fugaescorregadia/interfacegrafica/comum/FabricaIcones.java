ab9448cd538b32b7a5b86104d4e877804ea419ba
André Willik Valenti <awvalenti@gmail.com>
2015-07-30 13:00:40 -0300

    Mais uma spritesheet

--- src/main/java/com/github/awvalenti/fugaescorregadia/interfacegrafica/comum/FabricaIcones.java
+++ src/main/java/com/github/awvalenti/fugaescorregadia/interfacegrafica/comum/FabricaIcones.java
@@ -20,7 +20,7 @@ public class FabricaIcones {
 	public FabricaIcones() {
 		try {
 			BufferedImage spritesheet = ImageIO.read(getClass().getResource(
-					"/imagens/elementos/spritesheet-papel-3.png"));
+					"/imagens/elementos/spritesheet-papel-4.png"));
 			int tamanhoIcone = tamanhoIcone();
 			for (Elemento e : Elemento.values()) {
 				BufferedImage sprite = spritesheet.getSubimage(e.ordinal() % LARGURA_SPRITE_SHEET
