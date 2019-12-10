818f4469bdaff7a0d8c66258eb8919589cafc5a6
Andre Willik Valenti <awvalenti@gmail.com>
2019-12-10 09:47:36 -0300

    Testing <Board> with Enzyme

--- src/components/Board.test.tsx
+++ src/components/Board.test.tsx
@@ -2,12 +2,19 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import Board from './Board'
 
-import { describe, it } from 'mocha'
+import { describe, it, before, after } from 'mocha'
+import { expect } from 'chai'
+import { shallow, render, mount } from 'enzyme';
 
 describe(Board.name, () => {
   it('mounts and unmounts', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<Board />, div)
+    ReactDOM.render(<Board matrix={[[]]} />, div)
     ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('creates rows and columns', () => {
+    const wrapper = shallow(<Board matrix={[[]]} />)
+    expect(wrapper.html()).to.equal('<div><div></div></div>')
   })
 })
--- src/test/load-tests.ts
+++ src/test/load-tests.ts
@@ -1,2 +1,7 @@
-import '../../App.test';
-import '../components/Board.test';
+import '../../App.test'
+import '../components/Board.test'
+
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
