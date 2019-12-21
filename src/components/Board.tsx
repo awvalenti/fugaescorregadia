59eed79fc543219e028cae47f2f4cacc74d5083e
Andre Willik Valenti <awvalenti@gmail.com>
2019-12-21 18:26:29 -0200

    typeof instead of exported types

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,13 +2,11 @@ import * as React from 'react'
 import TileId from '../domain/TileId'
 import Tile from './Tile'
 
-export type Type = React.FC<{
+const Board: React.FC<{
 
   matrix: TileId[][]
 
-}>
-
-const Board: Type = ({ matrix }) =>
+}> = ({ matrix }) =>
   <div>{matrix.map((rowData, rowIndex) =>
     <div key={rowIndex}>{rowData.map((tileId, colIndex) =>
       <Tile key={colIndex} tileId={tileId} />)}
