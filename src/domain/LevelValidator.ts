37b31cc950401e05a0f145169c57da1cfc7a8a30
Andre Willik Valenti <awvalenti@gmail.com>
2019-12-28 17:44:27 -0200

    PLAYER, GOAL, LevelValidator

---
+++ src/domain/LevelValidator.ts
@@ -0,0 +1,20 @@
+import TileId from './TileId'
+
+const { PLAYER, GOAL } = TileId
+
+export default class LevelValidator {
+
+  run(matrix: TileId[][]) {
+    let playerCount = 0, goalCount = 0
+
+    matrix.forEach(rowData => {
+      rowData.forEach(tile => {
+        if (tile === PLAYER) ++playerCount
+        else if (tile === GOAL) ++goalCount
+      })
+    })
+
+    return playerCount === 1 && goalCount === 1
+  }
+
+}
