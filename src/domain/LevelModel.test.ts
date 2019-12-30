4662f4caa2144a1bcd5383d192de53b384dbe882
Andre Willik Valenti <awvalenti@gmail.com>
2019-12-30 00:46:51 -0200

    LevelModel#playerPos

--- src/domain/LevelModel.test.ts
+++ src/domain/LevelModel.test.ts
@@ -1,8 +1,9 @@
 import { expect } from 'chai'
 import LevelModel from './LevelModel'
-import { EMPTY, OBSTACLE } from './TileId'
+import { EMPTY, OBSTACLE, PLAYER } from './TileId'
 
 describe(LevelModel.name, () => {
+
   const [ooee1, ooee2, ee, oo] = [
       [[OBSTACLE, OBSTACLE], [EMPTY, EMPTY]],
       [[OBSTACLE, OBSTACLE], [EMPTY, EMPTY]],
@@ -10,11 +11,28 @@ describe(LevelModel.name, () => {
       [[OBSTACLE, OBSTACLE]],
   ].map(tileMatrix => new LevelModel(tileMatrix))
 
-  it('deep equals an equal object', () => {
-    expect(ooee1).to.deep.equal(ooee2)
+  describe('equality', () => {
+    it('deep equals an equal object', () => {
+      expect(ooee1).to.deep.equal(ooee2)
+    })
+
+    it('does not deep equal a different object', () => {
+      expect(ee).not.to.deep.equal(oo)
+    })
   })
 
-  it('does not deep equal a different object', () => {
-    expect(ee).not.to.deep.equal(oo)
+  describe('#playerPos', () => {
+    context('for valid level', () => {
+      it('finds position of PLAYER', () => {
+        expect(new LevelModel([[EMPTY, PLAYER]])).to.have.deep.property(
+          'playerPos', { row: 0, col: 1 })
+      })
+    })
+
+    context('for invalid level', () => {
+      it('returns null', () => {
+        expect(new LevelModel([])).to.have.property('playerPos', null)
+      })
+    })
   })
 })
--- src/domain/LevelModel.ts
+++ src/domain/LevelModel.ts
@@ -1,4 +1,4 @@
-import TileId from "./TileId";
+import TileId, { PLAYER } from "./TileId";
 
 export default class LevelModel {
 
@@ -8,4 +8,13 @@ export default class LevelModel {
     this.matrix = matrix
   }
 
+  get playerPos() {
+    for (let row = 0; row < this.matrix.length; ++row) {
+      for (let col = 0; col < this.matrix[row].length; ++col) {
+        if (this.matrix[row][col] === PLAYER) return { row, col }
+      }
+    }
+
+    return null
+  }
 }
