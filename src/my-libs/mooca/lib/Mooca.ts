79d986af7fc40d991be50dd6b17b31cd9db883a1
Andre Willik Valenti <awvalenti@gmail.com>
2020-01-07 09:57:07 -0200

    Mooca: method overload to stub default export

--- src/my-libs/mooca/lib/Mooca.ts
+++ src/my-libs/mooca/lib/Mooca.ts
@@ -3,16 +3,31 @@ export default class Mooca {
   private originals: Map<[any, string], any> = new Map
 
   stub<T extends object, K extends keyof T & string>(
-    module: T, exportName: K, newValue: T[K]): void {
-    const oldValue = module[exportName]
-    this.originals.set([module, exportName], oldValue)
-    module[exportName] = newValue
+    module: T, exportName: K, newValue: T[K]): void
+
+  stub<T extends object, K extends keyof T & string>(
+    module: T, newValue: T[K]): void
+
+  stub<T extends { default: any }, K extends keyof T & string>(
+    module: T, arg1: K | T[K], arg2?: T[K]): void {
+    if (arg2 !== undefined) {
+      this._stub(module, arg1 as K, arg2)
+    } else {
+      this._stub(module, 'default', arg1 as T[K])
+    }
   }
 
   restore(): void {
     this.originals.forEach((originalValue, [module, exportName]) => {
       module[exportName] = originalValue
     })
+  }
+
+  _stub<T extends object, K extends keyof T & string>(
+    module: T, exportName: K, newValue: T[K]): void {
+    const oldValue = module[exportName]
+    this.originals.set([module, exportName], oldValue)
+    module[exportName] = newValue
   }
 
 }
