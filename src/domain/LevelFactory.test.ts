75bb989d3bf8bfae3101b9737a486e5858ff250a
Andre Willik Valenti <awvalenti@gmail.com>
2020-02-28 22:31:39 -0300

    LevelFactory; eslint rules; organize imports on save

---
+++ src/domain/LevelFactory.test.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai'
+import LevelFactory from './LevelFactory'
+import { EMPTY, OBSTACLE } from './TileId'
+
+describe(LevelFactory.name, () => {
+  context('for valid input', () => {
+    it(`converts string to tile matrix`, () => {
+      expect((new LevelFactory).convert('- - - -\no o o o'))
+        .to.deep.equal([
+          [EMPTY, EMPTY, EMPTY, EMPTY],
+          [OBSTACLE, OBSTACLE, OBSTACLE, OBSTACLE],
+        ])
+    })
+  })
+
+  context('for invalid input', () => {
+    it(`throws error informing invalid character`, () => {
+      expect(() => (new LevelFactory).convert('- - - -\no o ? o'))
+        .to.throw(Error, 'Invalid character: ?')
+    })
+  })
+})
