5dd4a7df563e54d9dfe2311f027452db0e1c4bd2
Andre Willik Valenti <awvalenti@gmail.com>
2020-02-28 22:31:39 -0300

    LevelFactory; eslint rules; organize imports on save

---
+++ src/domain/LevelFactory.ts
@@ -0,0 +1,18 @@
+import TileId, { EMPTY, OBSTACLE } from './TileId'
+
+export default class LevelFactory {
+  convert(levelAsString: string): TileId[][] {
+    return levelAsString
+      .split('\n')
+      .map(rowString => rowString
+        .split(' ')
+        .map(tileCharCandidate => {
+          switch (tileCharCandidate) {
+            case '-': return EMPTY
+            case 'o': return OBSTACLE
+            default: throw Error(`Invalid character: ${tileCharCandidate}`)
+          }
+        })
+      )
+  }
+}
