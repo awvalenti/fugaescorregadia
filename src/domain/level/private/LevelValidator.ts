d14d76294852fb7ea65e6495b78422a73fd7f827
Andre Willik Valenti <awvalenti@gmail.com>
2020-03-14 14:27:08 -0300

    LevelFactory.test using ts-mockito

--- src/domain/level/private/LevelValidator.ts
+++ src/domain/level/private/LevelValidator.ts
@@ -1,23 +1,24 @@
 import TileId, { GOAL, PLAYER } from '../../TileId'
 
-const _consistentColsCount = (m: TileId[][]) =>
-  m.length <= 1 || m.every(row => row.length === m[0].length)
-
-const _correctAmounts = (matrix: TileId[][]) => {
-  let playerCount = 0, goalCount = 0
-
-  matrix.forEach(rowData => {
-    rowData.forEach(tile => {
-      if (tile === PLAYER) ++playerCount
-      else if (tile === GOAL) ++goalCount
-    })
-  })
-
-  return playerCount === 1 && goalCount === 1
-}
-
 export default class LevelValidator {
+  private _consistentColsCount(m: TileId[][]) {
+    return m.length <= 1 || m.every(row => row.length === m[0].length)
+  }
+
+  private _correctAmounts(matrix: TileId[][]) {
+    let playerCount = 0, goalCount = 0
+
+    matrix.forEach(rowData => {
+      rowData.forEach(tile => {
+        if (tile === PLAYER) ++playerCount
+        else if (tile === GOAL) ++goalCount
+      })
+    })
+
+    return playerCount === 1 && goalCount === 1
+  }
+
   run(matrix: TileId[][]): boolean {
-    return _consistentColsCount(matrix) && _correctAmounts(matrix)
+    return this._consistentColsCount(matrix) && this._correctAmounts(matrix)
   }
 }
