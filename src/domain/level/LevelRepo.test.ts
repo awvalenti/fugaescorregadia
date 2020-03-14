67942c949bf69b881b27fcb133f4bb3c672d56a9
Andre Willik Valenti <awvalenti@gmail.com>
2020-03-14 14:10:04 -0300

    LevelRepo

--- src/domain/level/LevelRepo.test.ts
+++ src/domain/level/LevelRepo.test.ts
@@ -1,19 +1,27 @@
 import { expect } from 'chai'
-import { EMPTY, OBSTACLE } from '../TileId'
+import { instance, mock, when } from 'ts-mockito'
+import LevelFactory from './LevelFactory'
 import LevelModel from './LevelModel'
 import LevelRepo from './LevelRepo'
+import LevelLoader from './private/LevelLoader'
 
 describe(LevelRepo.name, () => {
+
+  const level1234 = instance(mock(LevelModel))
+
   let repo: LevelRepo
 
   before(() => {
-    repo = new LevelRepo()
+    const LevelLoaderMock = mock(LevelLoader)
+    when(LevelLoaderMock.read('1234')).thenReturn('loaded-level')
+
+    const LevelFactoryMock = mock(LevelFactory)
+    when(LevelFactoryMock.create('loaded-level')).thenReturn(level1234)
+
+    repo = new LevelRepo(instance(LevelLoaderMock), instance(LevelFactoryMock))
   })
 
-  it('allows retrieving game levels', () => {
-    expect(repo.get()).to.deep.equal(new LevelModel([
-      [OBSTACLE, EMPTY, EMPTY, OBSTACLE],
-      [EMPTY, EMPTY, EMPTY, OBSTACLE],
-    ]))
+  it('allows retrieving game levels by id', () => {
+    expect(repo.get(1234)).to.equal(level1234)
   })
 })
