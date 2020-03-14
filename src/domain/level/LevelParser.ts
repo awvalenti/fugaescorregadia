15da25a517eafd443e3474d6fa25bb531a3699b1
Andre Willik Valenti <awvalenti@gmail.com>
2020-03-14 01:43:58 -0300

    LevelParser handling all TileIds

--- src/domain/level/LevelParser.ts
+++ src/domain/level/LevelParser.ts
@@ -1,4 +1,4 @@
-import TileId, { EMPTY, OBSTACLE } from '../TileId'
+import TileId, { EMPTY, GOAL, OBSTACLE, PLAYER } from '../TileId'
 
 export default class LevelParser {
   convert(levelAsString: string): TileId[][] {
@@ -6,10 +6,13 @@ export default class LevelParser {
       .split('\n')
       .map(rowString => rowString
         .split(' ')
+        // eslint-disable-next-line complexity
         .map(tileCharCandidate => {
           switch (tileCharCandidate) {
             case '-': return EMPTY
             case 'o': return OBSTACLE
+            case 'g': return GOAL
+            case 'p': return PLAYER
             default: throw Error(`Invalid character: ${tileCharCandidate}`)
           }
         })
