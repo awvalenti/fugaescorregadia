13a5d7ff5cc9cd311d424e5e0992e9f741271705
Andre Willik Valenti <awvalenti@gmail.com>
2020-03-14 01:07:09 -0300

    Improving rename script and adding it to package.json

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -5,13 +5,14 @@ if [ -z $1 ] || [ -z $2 ]; then
   exit 1
 fi
 
-find='find -name *.ts '
+find="find -regextype posix-extended -regex '.+\.(ts|tsx|js|jsx|json|yml|yaml)' "
 
-for dir in $(cat .gitignore); do
+for dir in $(cat .eslintignore); do
   find+="-not -path './$dir*' "
 done
 
-for file in $(eval $find); do
+for file in $(eval $find | xargs grep -l $1); do
+  echo $file
   sed -r "s/$1/$2/g" -i $file
   if [[ $file =~ $1 ]]; then
     mv $file ${file/$1/$2}
