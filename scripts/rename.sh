82d9761cbb53f269b4612ace838c7e8fe46aa802
Andre Willik Valenti <awvalenti@gmail.com>
2020-03-14 01:19:08 -0300

    new script

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -1,11 +1,11 @@
 set -e
 
 if [ -z $1 ] || [ -z $2 ]; then
-  >&2 echo "Please specify identifier source and destination names"
+  >&2 echo 'Please specify identifier source and destination names'
   exit 1
 fi
 
-find="find -regextype posix-extended -regex '.+\.(ts|tsx|js|jsx|json|yml|yaml)' "
+find='find -regextype posix-extended -regex ".+\.(ts|tsx|js|jsx|json|yml|yaml)" '
 
 for dir in $(cat .eslintignore); do
   find+="-not -path './$dir*' "
