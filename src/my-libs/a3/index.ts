9ddd817d786d73f127d5451559a181644e30f49c
Andre Willik Valenti <andre.valenti@lambda3.com.br>
2020-04-08 10:06:49 -0300

    Adding 'each' feature to a3; using it on LevelValidator

--- src/my-libs/a3/index.ts
+++ src/my-libs/a3/index.ts
@@ -1,18 +1,17 @@
-export function a3<T>(
-  sut: (new (...args: any) => T),
-  testSpecification: {
-    [key: string]: {
-      arrange: () => any
-      act: (arg0: any) => any
-      assert: {
-        [key: string]: (arg0: any) => void
-      }
+type TestSpec = {
+  [key: string]: {
+    arrange: () => any
+    act: (arg0: any) => any
+    assert: {
+      [key: string]: (arg0: any) => void
     }
   }
-) {
+}
+
+function _a3<T>(sut: new (...args: any) => T, testSpec: TestSpec) {
   describe(sut.name, () => {
-    Object.keys(testSpecification).forEach(ctx => {
-      const { arrange, act, assert } = testSpecification[ctx]
+    Object.keys(testSpec).forEach(ctx => {
+      const { arrange, act, assert } = testSpec[ctx]
 
       context(ctx, () => {
         let acted: any
@@ -30,4 +29,31 @@ export function a3<T>(
       })
     })
   })
+}
+
+export function a3<Sut>(
+  sut: new (...args: any) => Sut,
+  testSpec: TestSpec,
+): void
+
+export function a3<Sut, TestCase>(
+  sut: new (...args: any) => Sut,
+  testCases: TestCase[],
+  testCaseToTestSpecification: (arg0: TestCase) => TestSpec,
+): void
+
+export function a3<Sut, TestCase>(
+  sut: new (...args: any) => Sut,
+  testSpecOrTestCases: TestSpec | TestCase[],
+  testSpecFactory?: (arg0: TestCase) => TestSpec,
+) {
+  // Using local variable to avoid inline typecast, which
+  // would cause a bug in eslint. See:
+  // https://github.com/eslint/eslint/issues/13159
+  const testCases = testSpecOrTestCases as TestCase[]
+
+  _a3(sut, testSpecFactory === undefined
+    ? testSpecOrTestCases
+    : Object.assign({}, ...testCases.map(testSpecFactory))
+  )
 }
