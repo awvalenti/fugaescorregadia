d6bb69bcfa431fbe731451823165dd42efa19cad
Andre Willik Valenti <awvalenti@gmail.com>
2020-04-08 10:06:49 -0300

    Adding 'each' feature to a3; using it on LevelValidator

--- src/domain/level/private/LevelValidator.test.ts
+++ src/domain/level/private/LevelValidator.test.ts
@@ -1,28 +1,25 @@
 import { expect } from 'chai'
+import { a3 } from '../../../my-libs/a3'
 import TileId, { EMPTY, GOAL, OBSTACLE, PLAYER } from '../../TileId'
 import LevelValidator from './LevelValidator'
 
-describe(LevelValidator.name, () => {
+const testCases: [string, string, TileId[][]][] = [
+  ['accepts', `one ${PLAYER} and one ${GOAL}`, [[PLAYER, GOAL], [OBSTACLE, EMPTY]]],
+  ['rejects', 'inconsistent cols count', [[PLAYER, GOAL], [OBSTACLE], [EMPTY, EMPTY]]],
+  ['rejects', `no ${PLAYER}`, [[GOAL, EMPTY], [OBSTACLE, OBSTACLE]]],
+  ['rejects', `more than one ${PLAYER}`, [[PLAYER, PLAYER], [GOAL, OBSTACLE]]],
+  ['rejects', `no ${GOAL}`, [[PLAYER, EMPTY], [OBSTACLE, OBSTACLE]]],
+  ['rejects', `more than one ${GOAL}`, [[GOAL, GOAL], [PLAYER, OBSTACLE]]],
+]
 
-  const testCases: [string, string, TileId[][]][] = [
-    ['accepts', `one ${PLAYER} and one ${GOAL}`, [[PLAYER, GOAL], [OBSTACLE, EMPTY]]],
-    ['rejects', 'inconsistent cols count', [[PLAYER, GOAL], [OBSTACLE], [EMPTY, EMPTY]]],
-    ['rejects', `no ${PLAYER}`, [[GOAL, EMPTY], [OBSTACLE, OBSTACLE]]],
-    ['rejects', `more than one ${PLAYER}`, [[PLAYER, PLAYER], [GOAL, OBSTACLE]]],
-    ['rejects', `no ${GOAL}`, [[PLAYER, EMPTY], [OBSTACLE, OBSTACLE]]],
-    ['rejects', `more than one ${GOAL}`, [[GOAL, GOAL], [PLAYER, OBSTACLE]]],
-  ]
-
-  let validator: LevelValidator
-
-  before(() => {
-    validator = new LevelValidator()
-  })
-
-  testCases.forEach(([acceptsOrRejects, scenario, matrix]) => {
-    it(`${acceptsOrRejects} level with ${scenario}`, () => {
-      expect(validator.run(matrix)).to.equal(acceptsOrRejects === 'accepts')
-    })
-  })
-
-})
+a3(LevelValidator, testCases, ([acceptsOrRejects, scenario, matrix]) => ({
+  [`for ${scenario}`]: {
+    arrange: () => new LevelValidator(),
+    act: sut => sut.run(matrix),
+    assert: {
+      [`${acceptsOrRejects} level`]: result => {
+        expect(result).to.equal(acceptsOrRejects === 'accepts')
+      },
+    },
+  },
+}))
