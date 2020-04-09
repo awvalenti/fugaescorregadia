8fa166a87b7e364074c09938f6936da486f693bb
Andre Willik Valenti <andre.valenti@lambda3.com.br>
2020-04-09 10:47:21 -0300

    LevelModel.test using a3; nestable TestSpec's

--- src/my-libs/a3/index.ts
+++ src/my-libs/a3/index.ts
@@ -1,34 +1,52 @@
-type TestSpec = {
-  [key: string]: {
-    arrange: () => any
-    act: (arg0: any) => any
-    assert: {
-      [key: string]: (arg0: any) => void
-    }
-  }
+type TestSpec = Internal | Leaf
+
+type Internal = {
+  [key: string]: TestSpec
 }
 
-function _a3<T>(sut: new (...args: any) => T, testSpec: TestSpec) {
-  describe(sut.name, () => {
-    Object.keys(testSpec).forEach(ctx => {
-      const { arrange, act, assert } = testSpec[ctx]
+type Leaf = {
+  arrange?: () => any
+  act?: (arg0: any) => any
+  assert: { [key: string]: (arg0: any) => void }
+}
 
-      context(ctx, () => {
-        let acted: any
-        before(() => {
-          const arranged = arrange()
-          acted = act(arranged)
-        })
+function __a3(
+  sut: string,
+  node: TestSpec,
+) {
+  describe(sut, () => {
+    const isLeaf = (n: TestSpec): n is Leaf => 'assert' in n
 
-        Object.keys(assert).forEach(assertTitle => {
-          const assertFn = assert[assertTitle]
-          it(assertTitle, () => {
-            assertFn(acted)
-          })
+    if (isLeaf(node)) {
+      const { arrange, act, assert } = node
+
+      let acted: any
+
+      if (arrange || act) before(() => {
+        const arranged = arrange && arrange()
+        acted = act ? act(arranged) : arranged
+      })
+
+      Object.keys(assert).forEach(assertTitle => {
+        const assertFn = assert[assertTitle]
+        it(assertTitle, () => {
+          assertFn(acted)
         })
       })
-    })
+
+    } else {
+      Object.keys(node).forEach(name => {
+        __a3(name, node[name])
+      })
+    }
   })
+}
+
+function _a3<T>(
+  sut: new (...args: any) => T,
+  testSpec: TestSpec
+) {
+  __a3(sut.name, testSpec)
 }
 
 export function a3<Sut>(
