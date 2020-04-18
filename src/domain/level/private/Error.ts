96db19bfb324d2244965027b8557d169a92d3a52
Andre Willik Valenti <awvalenti@gmail.com>
2020-04-18 19:43:23 -0300

    Using real errors instead of strings

--- src/domain/level/private/Error.ts
+++ src/domain/level/private/Error.ts
@@ -1,11 +1,2 @@
-enum Error {
-  NO_PLAYER = 'NO_PLAYER',
-  INVALID_LEVEL = 'INVALID_LEVEL',
-}
-
-export const {
-  NO_PLAYER,
-  INVALID_LEVEL,
-} = Error
-
-export default Error
+export const NO_PLAYER = Error('NO_PLAYER')
+export const INVALID_LEVEL = Error('INVALID_LEVEL')
