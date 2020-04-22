f16a1c8e7bcc2dcdddcf5e753b9b3c73e81e9d51
Andre Willik Valenti <awvalenti@gmail.com>
2020-04-22 06:49:07 -0300

    wip a3 each

--- src/my-libs/a3/index.ts
+++ src/my-libs/a3/index.ts
@@ -1,6 +1,7 @@
 import nameof from '../nameof'
 
-type TestSpec<Arranged, Acted> = Internal<Arranged, Acted> | Leaf<Arranged, Acted>
+type TestSpec<Arranged, Acted> =
+  Internal<Arranged, Acted> | Leaf<Arranged, Acted>
 
 type Internal<Arranged, Acted> = {
   [key: string]: TestSpec<Arranged, Acted>
@@ -19,32 +20,35 @@ const isLeaf = (n: TestSpecAny): n is Leaf<any, any> => 'assert' in n
 
 function __a3(
   sut: string,
-  node: TestSpecAny,
+  nodeOrArray: TestSpecAny | TestSpecAny[],
 ) {
+  // eslint-disable-next-line complexity
   describe(sut, () => {
-    if (isLeaf(node)) {
-      const { arrange, act, assert, after: afterBlock } = node
+    for (const node of Array.isArray(nodeOrArray) ? nodeOrArray : [nodeOrArray]) {
+      if (isLeaf(node)) {
+        const { arrange, act, assert, after: afterBlock } = node
 
-      let arranged: any, acted: any
+        let arranged: any, acted: any
 
-      if (arrange || act) before(() => {
-        arranged = arrange && arrange()
-        acted = act ? act(arranged) : arranged
-      })
-
-      Object.keys(assert).forEach(assertTitle => {
-        const assertFn = assert[assertTitle]
-        it(assertTitle, () => {
-          assertFn(acted)
+        if (arrange || act) before(() => {
+          arranged = arrange && arrange()
+          acted = act ? act(arranged) : arranged
         })
-      })
 
-      if (afterBlock) after(() => afterBlock(arranged))
+        Object.keys(assert).forEach(assertTitle => {
+          const assertFn = assert[assertTitle]
+          it(assertTitle, () => {
+            assertFn(acted)
+          })
+        })
 
-    } else {
-      Object.keys(node).forEach(name => {
-        __a3(name, node[name])
-      })
+        if (afterBlock) after(() => afterBlock(arranged))
+
+      } else {
+        Object.keys(node).forEach(name => {
+          __a3(name, node[name])
+        })
+      }
     }
   })
 }
@@ -63,9 +67,33 @@ function privateA3<Sut extends Function, TestCase>(
 ) {
   _a3(sut, testSpecFactory === undefined
     ? testSpecOrTestCases as TestSpecAny
-    : Object.assign({}, ...(testSpecOrTestCases as TestCase[]).map(testSpecFactory))
+    : Object.assign({}, ...(testSpecOrTestCases as TestCase[])
+      .map(testSpecFactory))
   )
 }
+
+// export const each = <TestCaseData>(
+//   testCases: TestCaseData[],
+//   testSpecFactory: (testCase: TestCaseData) => TestSpecAny,
+// ) => testCases.map(current => testSpecFactory(current))
+
+export const each = <TestCaseData>(
+  testCases: TestCaseData[],
+  testSpecFactory: (testCase: TestCaseData) => TestSpecAny,
+) => testCases.reduce((ret, testCase) => {
+    const obj = testSpecFactory(testCase)
+    Object.keys(obj).forEach(key => {
+      if (!(key in ret)) ret[key] = []
+      ret[key].push(obj[key])
+    })
+    return ret
+  }, {})
+
+// export const each = <TestCaseData>(
+//   testCases: TestCaseData[],
+//   testSpecFactory: (testCase: TestCaseData) => TestSpecAny,
+// ) => testCases.reduce((ret, current) => !Object.keys(current).some(key => key in ret) && Object.assign(ret,
+//     testSpecFactory(current)), {})
 
 export function a3<Sut extends Function>(
   sut: Sut,
