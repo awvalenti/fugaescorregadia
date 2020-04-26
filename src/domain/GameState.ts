653fcdbb6e10c96b9cd816d87ba49ca9c85c868c
Andre Willik Valenti <andre.valenti@lambda3.com.br>
2020-04-26 10:16:55 -0300

    Level.prototype.inbounds

--- src/domain/GameState.ts
+++ src/domain/GameState.ts
@@ -1,19 +1,10 @@
 import Direction from './Direction'
 import Level from './level/Level'
 import Position from './Position'
-import TileId, { OBSTACLE } from './TileId'
-
-const inbounds = (matrix: TileId[][], row: number, col: number) =>
-  row >= 0 && row < matrix.length && col >= 0 && col < matrix[row].length
-
-const walk = (curPos: Position, background: TileId[][], dir: Position): Position => {
-  const newPos = { row: curPos.row + dir.row, col: curPos.col + dir.col }
-  return !inbounds(background, newPos.row, newPos.col) || background[newPos.row][newPos.col] === OBSTACLE
-    ? curPos
-    : walk(newPos, background, dir)
-}
+import { OBSTACLE } from './TileId'
 
 export default class GameState {
+
   private _level: Level
 
   constructor(level: Level) {
@@ -21,8 +12,17 @@ export default class GameState {
   }
 
   movePlayer(direction: Direction): Position {
-    const { playerPos, background } = this._level
-
-    return walk(playerPos, background, direction)
+    return this._movePlayer(this._level.playerPos, direction)
   }
+
+  private _movePlayer(curPos: Position, dir: Direction): Position {
+    const { background } = this._level
+
+    const newPos = { row: curPos.row + dir.row, col: curPos.col + dir.col }
+
+    return !this._level.inbounds(newPos) || background[newPos.row][newPos.col] === OBSTACLE
+      ? curPos
+      : this._movePlayer(newPos, dir)
+  }
+
 }
