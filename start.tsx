5d5e1c08a89cb828aa1fbd1d57fcc32bbbf870aa
Andre Willik Valenti <awvalenti@gmail.com>
2020-05-10 18:30:48 -0300

    Fixing start.tsx

--- start.tsx
+++ start.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import App from './src/components/App'
+import di from './src/di/prod'
 
-ReactDOM.render(<App />, document.getElementById('react-root'))
+ReactDOM.render(
+  <App level={di.levelRepo.get(0)} />,
+  document.getElementById('react-root')
+)
