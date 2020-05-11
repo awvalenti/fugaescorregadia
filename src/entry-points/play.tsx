52ffe500243897d4e89334e929882809ea959951
Andre Willik Valenti <awvalenti@gmail.com>
2020-05-10 23:07:37 -0300

    Single entry point for testing and playing

--- src/entry-points/play.tsx
+++ src/entry-points/play.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import App from './src/components/App'
-import di from './src/di/prod'
+import App from '../components/App'
+import di from '../di/prod'
 
 ReactDOM.render(
   <App level={di.levelRepo.get(0)} />,
---
+++ src/entry-points/test.tsx
@@ -0,0 +1,3 @@
+import '../test/1-setup-mocha'
+import '../test/2-load-tests'
+import '../test/3-run-tests'
