a0cf80543f13677da801f1dc52fce2e137e2385f
Andre Willik Valenti <andre.valenti@lambda3.com.br>
2020-05-13 20:57:25 -0300

    new.sh generating code snippets

--- scripts/new.sh
+++ scripts/new.sh
@@ -1,12 +1,17 @@
 set -e
 
-if [ -z $* ]; then
+if [ -z $1 ]; then
   >&2 echo 'Please specify file name'
   exit 1
 fi
 
 for file in $*; do
   path=src/$file
-  [[ $path =~ ((.+\/)*)(.+) ]] && mkdir -p ${BASH_REMATCH[2]}
-  touch $path.ts $path.test.ts
+
+  containing_folder=${BASH_REMATCH[2]}
+  [[ $path =~ ((.+\/)*)(.+) ]] && mkdir -p containing_folder
+
+  module_name=${BASH_REMATCH[3]}
+  printf "export default class $module_name {\n\n  HERE\n\n}\n" > $path.ts
+  printf "import { a3, cleanup, expect, Mooca, render } from './my-libs/my-testing-library'\nimport $module_name from './$module_name'\n\na3($module_name, {\n\n  HERE\n\n})\n" > $path.test.ts
 done
