62f8c1f660f4197115fe1c0c86631ee9d5a38578
Andre Willik Valenti <andre.valenti@lambda3.com.br>
2020-05-17 20:48:25 -0300

    Direction is now a class

--- src/domain/Direction.ts
+++ src/domain/Direction.ts
@@ -1,12 +1,23 @@
-export default interface Direction extends String {
-  rowInc: number
-  colInc: number
+export default class Direction {
+  private readonly _name: string
+
+  readonly rowInc: number
+  readonly colInc: number
+
+  private constructor(name: string, rowInc: number, colInc: number) {
+    this._name = name
+    this.rowInc = rowInc
+    this.colInc = colInc
+  }
+
+  toString(): string {
+    return this._name
+  }
+
+  static readonly LEFT = new Direction('LEFT', 0, -1)
+  static readonly UP = new Direction('UP', -1, 0)
+  static readonly RIGHT = new Direction('RIGHT', 0, 1)
+  static readonly DOWN = new Direction('DOWN', 1, 0)
 }
 
-const newDir = (name: string, rowInc: number, colInc: number): Direction =>
-  Object.assign(name, { rowInc, colInc })
-
-export const LEFT = newDir('LEFT', 0, -1)
-export const UP = newDir('UP', -1, 0)
-export const RIGHT = newDir('RIGHT', 0, 1)
-export const DOWN = newDir('DOWN', 1, 0)
+export const { LEFT, UP, RIGHT, DOWN } = Direction
