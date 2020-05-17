bc2db67015b1979525273eec6ccbfd9a8b3747b4
Andre Willik Valenti <awvalenti@gmail.com>
2020-05-17 20:48:25 -0300

    Direction is now a class

---
+++ src/domain/Direction.test.ts
@@ -0,0 +1,30 @@
+import { a3, each, expect } from '../my-libs/my-testing-library'
+import Direction, { DOWN, LEFT, RIGHT, UP } from './Direction'
+
+a3(Direction, {
+
+  ...each(<[Direction, string, number, number][]>[
+    [LEFT, 'LEFT', 0, -1],
+    [UP, 'UP', -1, 0],
+    [RIGHT, 'RIGHT', 0, 1],
+    [DOWN, 'DOWN', 1, 0],
+  ], ([direction, name, rowInc, colInc]) => ({
+    [name]: {
+      assert: {
+        'has correct rowInc': () => {
+          expect(direction.rowInc).to.equal(rowInc)
+        },
+
+        'has correct colInc': () => {
+          expect(direction.colInc).to.equal(colInc)
+        },
+
+        'gets converted correctly to string': () => {
+          expect(`${direction}`).to.equal(name)
+        },
+
+      },
+    },
+  })),
+
+})
