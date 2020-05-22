7d362947a01a436582a2972cc55b314ded91a6c7
Andre Willik Valenti <awvalenti@gmail.com>
2020-05-22 13:59:47 -0300

    <App> using keyboard and controller

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,44 +1,114 @@
-import { cleanup, render } from '@testing-library/react'
-import { expect } from 'chai'
 import * as React from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
+import { anyFunction, anything, instance, mock, verify } from 'ts-mockito'
 import GameState from '../domain/GameState'
 import Level from '../domain/level/Level'
 import Position from '../domain/Position'
-import { a3 } from '../my-libs/a3'
+import Controller from '../infra/Controller'
+import KeyboardInputHandler from '../infra/KeyboardInputHandler'
 import Mooca from '../my-libs/mooca'
+import { a3, cleanup, expect, render } from '../my-libs/my-testing-library'
 import nameof from '../my-libs/nameof'
 import App from './App'
 import * as Board from './Board'
 
+const arrange = () => {
+  const mooca = new Mooca()
+
+  mooca.stub(Board, ({ level, playerPos }) => <>{level},{playerPos}</>)
+
+  const
+    MockController = mock(Controller),
+    MockKeyboardInputHandler = mock(KeyboardInputHandler),
+
+    gameState = {
+      level: 'my-level' as unknown as Level,
+      playerPos: 'my-pos' as unknown as Position,
+    } as GameState,
+    controller = instance(MockController),
+    keyboardInputHandler = instance(MockKeyboardInputHandler)
+
+  return {
+    mooca,
+
+    MockController,
+    MockKeyboardInputHandler,
+
+    gameState,
+    controller,
+    keyboardInputHandler,
+  }
+}
+
+const mount = ({
+  gameState,
+  controller,
+  keyboardInputHandler,
+  ...rest
+}: any) => ({
+  component: render(<App
+    gameState={gameState}
+    controller={controller}
+    keyboardInputHandler={keyboardInputHandler}
+  />),
+  ...rest,
+})
+
+const after = ({ mooca }: any) => {
+  mooca.restore()
+  cleanup()
+}
+
 a3(App, {
-  arrange: () => {
-    const mooca = new Mooca()
+  'on mount': {
+    arrange,
 
-    mooca.stub(Board, ({ level, playerPos }) => <>{level},{playerPos}</>)
+    act: mount,
 
-    return {
-      mooca,
-      component: render(<App gameState={{
-        level: 'my-level' as unknown as Level,
-        playerPos: 'my-pos' as unknown as Position,
-      } as GameState} />),
-    }
-  },
+    assert: {
+      [`renders <main> with <${nameof(Board)}> using level and playerPos`]: ({
+        component,
+      }) => {
+        expect(component.container.innerHTML).to.equal(renderToStaticMarkup(
+          <main className="App">my-level,my-pos</main>
+        ))
+      },
 
-  act: ({ component }) => component.container.innerHTML,
+      [`calls ${nameof(Controller.prototype.setUpdateGameStateFn)}`]: ({
+        MockController,
+      }) => {
+        verify(MockController.setUpdateGameStateFn(anything())).once()
+        verify(MockController.setUpdateGameStateFn(anyFunction())).called()
+      },
 
-  assert: {
-    [`renders <main> with <${nameof(Board)}> using level and playerPos`]: innerHTML => {
-      expect(innerHTML).to.equal(renderToStaticMarkup(
-        <main className="App">my-level,my-pos</main>
-      ))
+      [`calls ${nameof(KeyboardInputHandler.prototype.enable)}`]: ({
+        MockKeyboardInputHandler,
+      }) => {
+        verify(MockKeyboardInputHandler.enable()).once()
+      },
     },
+
+    after,
   },
 
-  after: ({ mooca }) => {
-    mooca.restore()
-    cleanup()
+  'on umount': {
+    arrange,
+
+    act: arranged => {
+      const acted = mount(arranged)
+      acted.component.unmount()
+      return acted
+    },
+
+    assert: {
+      [`calls ${nameof(KeyboardInputHandler.prototype.disable)}`]: ({
+        MockKeyboardInputHandler,
+      }) => {
+        verify(MockKeyboardInputHandler.disable()).once()
+      },
+    },
+
+    after,
   },
 
 })
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import GameState from '../domain/GameState'
+import Controller from '../infra/Controller'
+import KeyboardInputHandler from '../infra/KeyboardInputHandler'
 import nameof from '../my-libs/nameof'
 import './App.sass'
 import Board from './Board'
@@ -7,10 +9,28 @@ import Board from './Board'
 const App: React.FC<{
 
   gameState: GameState
+  keyboardInputHandler: KeyboardInputHandler
+  controller: Controller
 
-}> = ({ gameState: { level, playerPos } }) =>
-  <main className={nameof(App)}>
+}> = ({
+  gameState,
+  controller,
+  keyboardInputHandler,
+}) => {
+  const [{ level, playerPos }, setGameState] = React.useState(gameState)
+
+  React.useEffect(() => {
+    controller.setUpdateGameStateFn(setGameState)
+  }, [controller])
+
+  React.useEffect(() => {
+    keyboardInputHandler.enable()
+    return () => keyboardInputHandler.disable()
+  }, [keyboardInputHandler])
+
+  return <main className={nameof(App)}>
     <Board level={level} playerPos={playerPos} />
   </main>
+}
 
 export default App
