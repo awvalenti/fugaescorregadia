badfab237e92c5e40f1cd61949490f06fd6e9874
Andre Willik Valenti <awvalenti@gmail.com>
2020-12-05 19:54:15 -0300

    myBind lib

--- src/infra/Controller.test.ts
+++ src/infra/Controller.test.ts
@@ -1,11 +1,34 @@
+import sinon from 'sinon'
 import { RIGHT } from '../domain/Direction'
 import GameState from '../domain/GameState'
+import * as myBind from '../my-libs/my-bind'
 import { myStub } from '../my-libs/my-stub'
-import { a3, expect } from '../my-libs/my-testing-library'
+import { a3, expect, Mooca } from '../my-libs/my-testing-library'
 import nameof from '../my-libs/nameof'
 import Controller, { NextGameStateFn } from './Controller'
 
 a3(Controller, {
+
+  constructor: {
+    arrange: () => {
+      const mooca = new Mooca()
+      const bindSpy = sinon.spy()
+      mooca.stub(myBind, bindSpy)
+      return { bindSpy, mooca }
+    },
+
+    act: (arranged: {}) => ({
+      ...arranged,
+      sut: new Controller(),
+    }),
+
+    assert: {
+      [`binds ${nameof(Controller.prototype.moveFinished$)}`]:
+      ({ sut, bindSpy }: any) => {
+        expect(bindSpy).to.have.been.calledOnceWithExactly(sut, 'moveFinished$')
+      },
+    },
+  },
 
   [nameof(Controller.prototype.dispatchMove$)]: {
     'when updateGameStateFn is NOT set': {
