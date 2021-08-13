69e2a9dc464c29d397bc8a1378ad66833844e7c3
Andre Willik Valenti <awvalenti@gmail.com>
2021-08-13 19:22:35 -0300

    rename.sh: double quoting variables

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -1,6 +1,6 @@
 set -e
 
-if [ -z $1 ] || [ -z $2 ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
   >&2 echo 'Please specify identifier source and destination names'
   exit 1
 fi
@@ -11,10 +11,10 @@ for dir in $(cat .eslintignore); do
   find+="-not -path './$dir*' "
 done
 
-for file in $(eval $find | xargs grep -l $1); do
-  echo $file
-  sed -r "s/$1/$2/g" -i $file
+for file in $(eval "$find" | xargs grep -l "$1"); do
+  echo "$file"
+  sed -r "s/$1/$2/g" -i "$file"
   if [[ $file =~ $1 ]]; then
-    mv $file ${file/$1/$2}
+    mv "$file" "${file/$1/$2}"
   fi
 done
