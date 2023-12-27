145e7003d956c345a6bb82f426d38be744c697fa
Andre Willik Valenti <awvalenti@gmail.com>
2023-12-27 12:49:35 -0300

    offline coding - MacSoundPlayer draft

--- sound-player/MacSoundPlayer.js
+++ sound-player/MacSoundPlayer.js
@@ -1,3 +1,31 @@
+import { spawn } from 'child_process'
+
+process.on('exit', () => {
+  spawn('killall', ['afplay'])
+})
+
 export class MacSoundPlayer {
+
+  async prefetch(soundFile) {
+    let afplayProcess
+
+    return {
+      start() {
+        afplayProcess = spawn('afplay', [soundFile])
+      },
+
+      stop() {
+        afplayProcess.kill('SIGTERM')
+      },
+
+      pause() {
+        afplayProcess.kill('SIGSTOP')
+      },
+
+      resume() {
+        afplayProcess.kill('SIGCONT')
+      },
+    }
+  }
 
 }
